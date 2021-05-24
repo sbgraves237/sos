a0f4be6ce89f1d082826d0db921caf1703597453
sbgraves237 <spencer.graves@effectivedefense.org>
2021-05-23 22:04:30 -0500

    del fda

--- R/writeFindFn2xls.R
+++ R/writeFindFn2xls.R
@@ -37,7 +37,7 @@ writeFindFn2xls <- function(x,
 ##
 ## Don't write a file for CRAN 
 ##
-  if(requireNamespace("fda") && fda::CRAN()){
+  if(fda::CRAN()){
     cat('write nothing on CRAN')
   } else {
 ##
