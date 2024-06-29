2a4fd6d56a54a1bb848717bb75f87bc5712cb05d
sbgraves237 <spencer.graves@effectivedefense.org>
2024-06-29 10:43:37 -0500

    update release process

--- sos_release_to_CRAN.R
+++ sos_release_to_CRAN.R
@@ -7,7 +7,7 @@
 
 library(usethis)
 #usethis::use_release_issue()
-use_release_issue()
+#use_release_issue()
 # I cannot make this work.  
 # Reported to Jenny Bryan, 2023-06-16.  
 
@@ -32,18 +32,18 @@ devtools::check_rhub()
 # install.packages("revdepcheck")
 #devtools::install_github('r-lib/revdepcheck')
 
-revdepcheck::revdep_reset(Pkg)
+revdepcheck::revdep_reset()
 
-revdepcheck::revdep_check(Pkg, num_workers = 4)
+revdepcheck::revdep_check(num_workers = 4)
 
 #20.4.  Update README.md and NEWS.md 
 
 #20.5.  Submit to CRAN
 
 library(devtools)
-spell_check(Pkg)
+spell_check()
 
-release(Pkg)
+release()
 
 #20.6.  Update the version number for the future 
 #20.7.  Publicise
